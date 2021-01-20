3b24624edf8b74d2ef6025cef2838cacee0119cb
Apple.Fell <aflyingpeiceofglue@gmail.com>
2021-01-20 12:08:30 -0700

    added a daily cash command

---
+++ commands/daily.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'daily',
+	description: 'Gives you some cash every 24 hours!',
+	cooldown: 86400,
+	// eslint-disable-next-line no-unused-vars
+	execute(message, args) {
+		const add = Math.floor(Math.random() * (100 - 1) + 1);
+		const { moners } = require('../index');
+
+		moners.add(message.author.id, add);
+		message.channel.send(`You found $${add}!`);
+	},
+};
